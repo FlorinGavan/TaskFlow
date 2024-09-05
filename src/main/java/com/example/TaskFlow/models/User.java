3132060e63265c8895be1ee31ef9fde9eb70aa0e
Sabin <sabins.badea@gmail.com>
2024-09-05 16:13:25 +0300

    Created User model class

---
+++ src/main/java/com/example/TaskFlow/models/User.java
@@ -0,0 +1,11 @@
+package com.example.TaskFlow.models;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class User {
+    private String firstName, lastName;
+    private UUID IDno;
+}
