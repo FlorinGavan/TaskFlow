71bf830fa650c8d30411ebf5b099a8ffb3aaf388
Ghita <iceman57187@gmail.com>
2024-09-11 00:17:44 +0300

    A little update on TaskRepository and TaskService

--- src/main/java/com/example/TaskFlow/repositories/TaskRepository.java
+++ src/main/java/com/example/TaskFlow/repositories/TaskRepository.java
@@ -1,6 +1,6 @@
 package com.example.TaskFlow.repositories;
 
-import com.example.TaskFlow.models.entitys.Task;
+import com.example.TaskFlow.models.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
